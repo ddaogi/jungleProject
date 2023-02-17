a738055c96f6fb0a02ee159f4e4ce0b64639ea13
ddaogi <47049582+ddaogi@users.noreply.github.com>
2023-02-17 23:26:30 +0900

    complete version with server setting

--- alonememo/app.py
+++ alonememo/app.py
@@ -1,25 +1,19 @@
 from flask import Flask, render_template, jsonify, request
-app = Flask(__name__)
-
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
 
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client.dbjungle
+app = Flask(__name__)
 
-## HTML을 주는 부분
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbjungle  # 'dbjungle'라는 이름의 db를 만들거나 사용합니다.
+
+
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/memo', methods=['GET'])
-def listing():
-    # 1. 모든 document 찾기 & _id 값은 출력에서 제외하기
-    # 2. articles라는 키 값으로 영화정보 내려주기
-    return jsonify({'result':'success', 'msg':'GET 연결되었습니다!'})
 
-## API 역할을 하는 부분
 @app.route('/memo', methods=['POST'])
 def post_article():
     # 1. 클라이언트로부터 데이터를 받기
@@ -47,5 +41,15 @@ def post_article():
     db.articles.insert_one(article)
 
     return jsonify({'result': 'success'})
+
+
+@app.route('/memo', methods=['GET'])
+def read_articles():
+    # 1. mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기 (Read)
+    result = list(db.articles.find({}, {'_id': 0}))
+    # 2. articles라는 키 값으로 article 정보 보내주기
+    return jsonify({'result': 'success', 'articles': result})
+
+
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
